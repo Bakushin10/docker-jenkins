fc6a6118b18c23f73e0de184ac983dc50275b31d
“YOUR <shnnagai@gmail.com>
2020-01-26 01:50:19 +0900

    feature(core) : craete Dockerfile for jenkins

--- security.groovy
+++ security.groovy
@@ -6,7 +6,12 @@ import jenkins.security.s2m.AdminWhitelistRule
 import jenkins.model.JenkinsLocationConfiguration
 import net.sf.json.JSONObject
 
+import jenkins.security.QueueItemAuthenticatorConfiguration
+import org.jenkinsci.plugins.authorizeproject.GlobalQueueItemAuthenticator
+import org.jenkinsci.plugins.authorizeproject.strategy.TriggeringUsersAuthorizationStrategy
+
 println("=== Configuring users")
+
 def instance = Jenkins.getInstance()
 String admin_email = 'raghuk.vit@gmail.com'
 JenkinsLocationConfiguration location = instance.getExtensionList('jenkins.model.JenkinsLocationConfiguration')[0]
@@ -23,3 +28,9 @@ println "Updating Jenkins Email to: ${admin_email}"
 location.adminAddress = admin_email
 instance.save()
 location.save()
+
+println("=== Configure Authorize Project")
+GlobalQueueItemAuthenticator auth = new GlobalQueueItemAuthenticator(
+    new TriggeringUsersAuthorizationStrategy()
+)
+QueueItemAuthenticatorConfiguration.get().authenticators.add(auth)
