a95dd4650e0589f344e4f5482e2f4003470f727b
“YOUR <shnnagai@gmail.com>
2020-01-27 23:39:09 +0900

    add push.sh

---
+++ push.sh
@@ -0,0 +1,13 @@
+git add .
+
+echo 'enter the commit message'
+read commitmessage
+
+git commit -m "$commitmessage"
+
+echo 'enter the name of the branch'
+read branch
+
+git push origin $branch
+
+read
